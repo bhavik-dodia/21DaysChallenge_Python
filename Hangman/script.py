8ce9f4c7153b575f7ffb5495701d419db0b90889
bhavikdodia2000 <dodiabhavik.db@gmail.com>
2020-03-28 15:44:52 +0530

    v1.0

---
+++ Hangman/script.py
@@ -0,0 +1,42 @@
+import time
+import random
+
+name = input('What is your name? ')
+print('\nHello, ' + name + ' time to play Hangman!')
+print()
+time.sleep(1)
+
+print('Start guessing...')
+time.sleep(0.5)
+
+words = ['rainbow', 'computer', 'science', 'programming', 'python', 'mathematics', 'player', 'condition', 'reverse', 'water', 'board', name]
+word = random.choice(words)
+guesses = ''
+turns = 10
+
+while turns > 0: 
+    failed = 0
+    for char in word:
+        if char in guesses:
+            print(char)
+        else:
+            print('_')
+            failed = 1
+    
+    if failed == 0:
+        print('Congratulations, You Win!!!')
+        print('The word is: ' + word)
+        print()
+        break
+    print()
+
+    guess = input('Guess a character: ')
+    guesses += guess
+
+    if guess not in word:
+        turns -= 1
+        print('\nOops! wrong guess')
+        print('You have ' + str(turns) + ' more guesses.\n')
+    
+    if turns == 0:
+        print('You loose')
