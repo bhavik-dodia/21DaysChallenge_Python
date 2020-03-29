c01ece8c9191f5e54c005594e2e8880a5b5ffbee
bhavikdodia2000 <dodiabhavik.db@gmail.com>
2020-03-29 23:12:23 +0530

    v1.1

--- Hangman/script.py
+++ Hangman/script.py
@@ -10,33 +10,37 @@ print('Start guessing...')
 time.sleep(0.5)
 
 words = ['rainbow', 'computer', 'science', 'programming', 'python', 'mathematics', 'player', 'condition', 'reverse', 'water', 'board', name]
-word = random.choice(words)
-guesses = ''
-turns = 10
 
-while turns > 0: 
-    failed = 0
-    for char in word:
-        if char in guesses:
-            print(char)
-        else:
-            print('_')
-            failed = 1
-    
-    if failed == 0:
-        print('Congratulations, You Win!!!')
-        print('The word is: ' + word)
+c = 'y'
+while c == 'y':
+    word = random.choice(words)
+    guesses = ''
+    turns = 10
+    while turns > 0: 
+        failed = 0
+        for char in word:
+            if char in guesses:
+                print(char)
+            else:
+                print('_')
+                failed = 1
+        
+        if failed == 0:
+            print('\nCongratulations, You Win!!!')
+            print('The word is: ' + word)
+            print()
+            break
         print()
-        break
-    print()
 
-    guess = input('Guess a character: ')
-    guesses += guess
+        guess = input('Guess a character: ')
+        guesses += guess
 
-    if guess not in word:
-        turns -= 1
-        print('\nOops! wrong guess')
-        print('You have ' + str(turns) + ' more guesses.\n')
+        if guess not in word:
+            turns -= 1
+            print('\nOops! wrong guess')
+            print('You have ' + str(turns) + ' more guesses.\n')
+        
+        if turns == 0:
+            print('You loose')
     
-    if turns == 0:
-        print('You loose')
+    c = input('\nDo you want to play again?(y/n) ')
