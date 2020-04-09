6df157aea5fe1635674a2a734c4d4e98f4451df1
bhavikdodia2000 <dodiabhavik.db@gmail.com>
2020-04-09 20:47:13 +0530

    __name__ variable

---
+++ Special variables/first_module.py
@@ -0,0 +1,7 @@
+print('This will always be run')
+
+def main():
+    print('First Module\'s main method')
+
+if __name__ == '__main__':
+    main()
---
+++ Special variables/second_module.py
@@ -0,0 +1,6 @@
+import first_module
+
+# to execute main method
+first_module.main()
+
+print(f'Second module\'s name: {__name__}')
